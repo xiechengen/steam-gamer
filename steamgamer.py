0066cc24dc49d9059ce57a60e19dd0d73c23889a
Dino <dinosaur3366@gmail.com>
2016-02-13 02:35:18 -0500

    add boostrap templates

--- steamgamer.py
+++ steamgamer.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
