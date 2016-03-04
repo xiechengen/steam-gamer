8ba77e6ecd63d5089b7437466a1016f04a0db701
Dino <dinosaur3366@gmail.com>
2016-03-04 15:14:25 -0500

    dino api output

--- steamapi.py
+++ steamapi.py
@@ -6,20 +6,20 @@ import csv
 games = Games()
 
 
-'''
+
 getall = games.get_all("CN")
 counter = 0
 with open("dino_output.csv", 'wb') as f:
         file_writer = csv.writer(f, delimiter=',')
         for i in getall:
             try:
-                print "#%d" % counter, " ",i.name.encode('utf-8'), i.price
-                file_writer.writerow([i.name , i.price])
+                print "#%d" % counter, i.appid,i.name.encode('utf-8'), i.price, i.store_url 
+                file_writer.writerow([i.appid, i.name , i.price, i.store_url])
             except:
                 continue
             counter += 1
-'''
 
 
 
-user = SteamUser(userid, 'FC65FE32E5732FC54BC70256CDA122BB')
+
+#user = SteamUser(userid, 'FC65FE32E5732FC54BC70256CDA122BB')
