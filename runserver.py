02b9198ca249f390bc0ca84a06a9a25ae8b389db
Dino <dinosaur3366@gmail.com>
2016-03-25 22:03:25 -0400

    changes to steam openid sign in

--- runserver.py
+++ runserver.py
@@ -3,11 +3,12 @@ from flask import Flask, request, session, g, redirect
 from flask import url_for, abort, render_template, flash
 import urllib2
 import json
+import re
 
 from steamapiwrapper.Users import SteamUser
 from steamapiwrapper.SteamGames import Games
 
-'''
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_openid import OpenID
 
@@ -15,7 +16,7 @@ app = Flask(__name__)
 #app.config.from_pyfile('settings.cfg')
 db = SQLAlchemy(app)
 oid = OpenID(app)
-'''
+
 # configuration
 DATABASE = 'steam-gamer.db'
 DEBUG = True
@@ -30,6 +31,19 @@ app.config.from_object(__name__)
 app.debug = True
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    steam_id = db.Column(db.String(40))
+    nickname = db.String(80)
+
+    @staticmethod
+    def get_or_create(steam_id):
+        rv = User.query.filter_by(steam_id=steam_id).first()
+        if rv is None:
+            rv = User()
+            rv.steam_id = steam_id
+            db.session.add(rv)
+        return rv
 
 '''
 def connect_db():
@@ -50,10 +64,58 @@ def teardown_request(exception):
 def index():
     return render_template('index.html')
 
-@app.route('/signin')
+
+@app.route('/signin', methods=['GET', 'POST'])
 def sigin():
     return render_template('signin.html')
 
+
+
+
+
+
+_steam_id_re = re.compile('steamcommunity.com/openid/id/(.*?)$')
+
+@app.route('/login')
+@oid.loginhandler
+def login():
+    print oid.get_next_url
+    if g.user is not None:
+        return redirect(oid.get_next_url())
+    return oid.try_login('http://steamcommunity.com/openid')
+
+
+def get_steam_userinfo(steam_id):
+    options = {
+        'key': 'FC65FE32E5732FC54BC70256CDA122BB',
+        'steamids': steam_id
+    }
+    url = 'http://api.steampowered.com/ISteamUser/' \
+          'GetPlayerSummaries/v0001/?%s' % url_encode(options)
+    rv = json.load(urllib2.urlopen(url))
+    return rv['response']['players']['player'][0] or {}
+
+@oid.after_login
+def create_or_login(resp):
+    match = _steam_id_re.search(resp.identity_url)
+    g.user = User.get_or_create(match.group(1))
+    steamdata = get_steam_userinfo(g.user.steam_id)
+    g.user.nickname = steamdata['personaname']
+    db.session.commit()
+    session['user_id'] = g.user.id
+    flash('You are logged in as %s' % g.user.nickname)
+    return redirect(oid.get_next_url())
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.get(session['user_id'])
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(oid.get_next_url())
 
 @app.route('/calculator', methods=['GET', 'POST'])
 def cal():
